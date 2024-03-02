2d396ca47b17a077743ad96b960ee835e60701e7
William Frisk <william.hp.frisk@gmail.com>
2024-03-02 13:05:24 +0100

    clean up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
