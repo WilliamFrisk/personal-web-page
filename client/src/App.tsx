58e487d3952f7f15be7d72750553de2521943662
William Frisk <william.hp.frisk@gmail.com>
2024-03-02 13:33:11 +0100

    Added placeholder pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
-import "./App.css";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import { Container } from "react-bootstrap";
 import Header from "./components/header/Header";
+import ContactPage from "./pages/contactpage/ContactPage";
+import WorkExperiencePage from "./pages/workexperiencepage/WorkExperiencePage";
+import EducationPage from "./pages/educationpage/EducationPage";
+import CvPage from "./pages/cvpage/CvPage";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/work" element={<WorkExperiencePage />} />
+          <Route path="/education" element={<EducationPage />} />
+          <Route path="/cv" element={<CvPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </Router>
     </Container>
---
+++ client/src/pages/cvpage/CvPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CvPage: React.FC = () => {
+  return (
+    <div className="h-100">
+      <h1 className="coming-soon">Coming soon...</h1>
+    </div>
+  );
+};
+
+export default CvPage;
